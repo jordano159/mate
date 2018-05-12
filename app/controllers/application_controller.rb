ac1edc6fcaa28db057127e74c621d2ec1202d995
Jordan <yarden11111@gmail.com>
2018-05-12 18:53:16 +0300

    clearence

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_staff!
   before_action :level
+  before_action :clearence
 
   private
 
@@ -13,6 +14,27 @@ class ApplicationController < ActionController::Base
       end
     else
       @level = 0
+    end
+  end
+
+  def clearence
+    case controller_name
+    when "axes"
+      if @level < 3
+        redirect_to root_path, notice: 'אין לך הרשאה לצפות בזה'
+      end
+    when "heads"
+      if @level < 2
+        redirect_to root_path, notice: 'אין לך הרשאה לצפות בזה'
+      end
+    when "groups"
+      if @level < 1
+        redirect_to root_path, notice: 'אין לך הרשאה לצפות בזה'
+      end
+    when "kids"
+      if @level < 1
+        redirect_to root_path, notice: 'אין לך הרשאה לצפות בזה'
+      end
     end
   end
 end
