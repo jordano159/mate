7207e0388ff85299c9c9edd761022e16d5b62b10
Jordan <yarden11111@gmail.com>
2018-06-25 17:08:58 +0300

    seed staff

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -17,15 +17,15 @@ class Kid < ApplicationRecord
     end
 
   def self.search(search_term)
-    Kid.where(
+    Kid.joins(:group).where(
       "kids.name LIKE ? OR kids.sex LIKE ? OR kids.last_name LIKE ? OR kids.phone LIKE ? OR
       kids.medical LIKE ? OR kids.meds LIKE ? OR kids.food LIKE ? OR kids.city LIKE ? OR
       kids.ken LIKE ? OR kids.dad LIKE ? OR kids.dad_phone LIKE ? OR kids.mom LIKE ? OR
-      kids.mom_phone LIKE ? OR kids.size LIKE ?",
+      kids.mom_phone LIKE ? OR kids.size LIKE ? OR groups.name LIKE ?",
       "%#{search_term}%", "%#{search_term}%", "%#{search_term}%", "%#{search_term}%",
       "%#{search_term}%", "%#{search_term}%", "%#{search_term}%", "%#{search_term}%",
       "%#{search_term}%", "%#{search_term}%", "%#{search_term}%", "%#{search_term}%",
-      "%#{search_term}%", "%#{search_term}%").distinct
+      "%#{search_term}%", "%#{search_term}%", "%#{search_term}%").distinct
   end
 
   def self.to_csv
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 puts "Creating Axes..."
 for i in 1..2
   Axis.create!(name: "#{i}") if Axis.find_by(name: "#{i}").nil?
+  Staff.create!(name: "רכזת ציר #{i}", email: "a#{i}@gmail.com", password: "123123", password_confirmation: "123123",
+    role: "user", username: "ציר #{i}", staffable_type: "Axis", staffable_id: Axis.find_by(name: "#{i}").id) if Staff.find_by(username: "ציר #{i}").nil?
 end
 
 # Seeding six Heads
@@ -16,13 +18,16 @@ for i in 1..6
   elsif i >= 4
     Head.create!(name: "#{i}", axis_id: "#{Axis.find_by(name: "2").id}") if Head.find_by(name: "#{i}").nil?
   end
+  Staff.create!(name: "ראשראשית #{i}", email: "h#{i}@gmail.com", password: "123123", password_confirmation: "123123",
+    role: "user", username: "ראש #{i}", staffable_type: "Head", staffable_id: Head.find_by(name: "#{i}").id) if Staff.find_by(username: "ראש #{i}").nil?
 end
 
 # Seeding twenty Groups
 puts "Creating Groups..."
-ActiveRecord::Base.connection.reset_pk_sequence!('groups') 
 for i in 1..20
   Group.create!(name: "#{i}", head_id: "#{Head.find_by(name: "#{i/5}").id}") if Group.find_by(name: "#{i}").nil?
+  Staff.create!(name: "מדריכת #{i}", email: "g#{i}@gmail.com", password: "123123", password_confirmation: "123123",
+    role: "user", username: "קבוצה #{i}", staffable_type: "Group", staffable_id: Group.find_by(name: "#{i}").id) if Staff.find_by(username: "קבוצה #{i}").nil?
 end
 
 # Seeding Admin
