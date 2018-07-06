2a8651ac28711379adfcd8fe5fc908fcd132473f
Jordan <yarden11111@gmail.com>
2018-07-06 11:06:44 +0300

    head group index, dynamic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,19 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 
-# Seeding two Axes
-puts "Creating Axes..."
-for i in 1..2
+# Seeding Axes
+puts "How Many Axes?"
+amount = STDIN.gets.chomp.to_i
+for i in 1..amount
   Axis.create!(name: "#{i}") if Axis.find_by(name: "#{i}").nil?
   Staff.create!(name: "רכזת ציר #{i}", email: "a#{i}@gmail.com", password: "123123", password_confirmation: "123123",
     role: "user", username: "ציר #{i}", staffable_type: "Axis", staffable_id: Axis.find_by(name: "#{i}").id) if Staff.find_by(username: "ציר #{i}").nil?
 end
 
-# Seeding six Heads
-puts "Creating Heads..."
-for i in 1..6
+# Seeding Heads
+puts "How Many Heads?"
+amount = STDIN.gets.chomp.to_i
+for i in 1..amount
   if i < 4
     Head.create!(name: "#{i}", axis_id: "#{Axis.find_by(name: "1").id}") if Head.find_by(name: "#{i}").nil?
   elsif i >= 4
@@ -22,9 +24,10 @@ for i in 1..6
     role: "user", username: "ראש #{i}", staffable_type: "Head", staffable_id: Head.find_by(name: "#{i}").id) if Staff.find_by(username: "ראש #{i}").nil?
 end
 
-# Seeding twenty Groups
-puts "Creating Groups..."
-for i in 1..20
+# Seeding Groups
+puts "How Many Groups?"
+amount = STDIN.gets.chomp.to_i
+for i in 1..amount
   Group.create!(name: "#{i}", head_id: "#{Head.find_by(name: "#{i/5}").id}") if Group.find_by(name: "#{i}").nil?
   Staff.create!(name: "מדריכת #{i}", email: "g#{i}@gmail.com", password: "123123", password_confirmation: "123123",
     role: "user", username: "קבוצה #{i}", staffable_type: "Group", staffable_id: Group.find_by(name: "#{i}").id) if Staff.find_by(username: "קבוצה #{i}").nil?
