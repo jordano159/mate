9a1c0272979869f4a2742e5ea2ee8d3b8f2738a9
Jordan <yarden11111@gmail.com>
2018-07-23 10:12:28 +0300

    how many attending fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def how_many_attending(object)
     counter = 0
     object.each do |kid|
-      counter += kid.status
+      counter += kid.status if !kid.status.nil?
     end
     counter
   end
