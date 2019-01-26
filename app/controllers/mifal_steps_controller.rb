62cd939fdc8e2cb73bd2a51a6b3bdb87d13cb240
Jordan Shefner <yarden11111@gmail.com>
2019-01-26 16:31:57 +0200

    Better headed in wizard

--- app/controllers/mifal_steps_controller.rb
+++ app/controllers/mifal_steps_controller.rb
@@ -1,5 +1,6 @@
 class MifalStepsController < ApplicationController
   include Wicked::Wizard
+  before_action :check_permission
 
   steps :axised, :headed, :grouped, :imported_kids, :done
 
@@ -17,6 +18,7 @@ class MifalStepsController < ApplicationController
     @mifal = current_staff.staffable
     case step
     when :axised
+      @mifal.axised! # Set stage
       axis_num = params[:mifal][:axis_num].to_i
       (1..axis_num).each do |i|
         Axis.create(name: "ציר #{i} #{@mifal.name}", mifal_id: @mifal.id) if Axis.find_by(name: "ציר #{i} #{@mifal.name}").nil?
@@ -27,18 +29,25 @@ class MifalStepsController < ApplicationController
           staffable_id: Axis.find_by(name: "ציר #{i} #{@mifal.name}").id)
       end
     when :headed
-      head_num = params[:mifal][:head_num].to_i
-      head_in_axis_num = params[:mifal][:head_in_axis_num].to_i
-      (1..head_num).each do |i|
-        Head.create(name: "ראש #{i} #{@mifal.name}",
-        axis_id: Axis.find_by(name: "ציר #{(i / (head_in_axis_num + 1)) + 1} #{@mifal.name}").id.to_s) if Head.find_by(name: "ראש #{i} #{@mifal.name}").nil?
-        next unless Staff.find_by(username: "ראש #{i} #{@mifal.name}").nil?
-        #בעיה בחישוב של שיוך הראש לציר הנכון
-        Staff.create(name: "ראשראשית #{i} #{@mifal.name}", email: "h#{@mifal.name}#{i}@gmail.com", password: '123123',
-          password_confirmation: '123123', role: 'user', username: "ראש #{i} #{@mifal.name}", staffable_type: 'Head',
-          staffable_id: Head.find_by(name: "ראש #{i} #{@mifal.name}").id)
+      @mifal.headed! # Set stage
+      head_nums = []
+      @mifal.axes.count.times do |i|
+        head_nums << params[:mifal]["head_num_#{i}"].to_i
+      end
+      counter = 1
+      @mifal.axes.each_with_index do |axis,i| # ?לאחד את שתי הלולאות
+        head_nums[i].times do |j|
+          Head.create(name: "ראש #{counter} #{@mifal.name}", axis_id: axis.id)
+          next unless Staff.find_by(username: "ראש #{counter} #{@mifal.name}").nil?
+
+          Staff.create(name: "ראשראשית #{counter} #{@mifal.name}", email: "h#{@mifal.name}#{counter}@gmail.com", password: '123123',
+            password_confirmation: '123123', role: 'user', username: "ראש #{counter} #{@mifal.name}", staffable_type: 'Head',
+            staffable_id: Head.find_by(name: "ראש #{counter} #{@mifal.name}").id)
+          counter += 1
+        end
       end
     when :grouped
+      @mifal.grouped! # Set stage
       group_num = params[:mifal][:group_num].to_i
       group_in_head_num = params[:mifal][:group_in_head_num].to_i
       (1..group_num).each do |i|
@@ -56,6 +65,11 @@ class MifalStepsController < ApplicationController
   end
 
   private
+    def check_permission
+      unless current_staff.admin? || current_staff.vip?
+        redirect_to root_path
+      end
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def mifal_params
       params.require(:mifal).permit(:name, axis_ids: [])
--- app/models/mifal.rb
+++ app/models/mifal.rb
@@ -7,4 +7,5 @@ class Mifal < ApplicationRecord
   has_many :checks, through: :axes, dependent: :destroy
   has_many :events, as: :eventable
   has_many :buses, dependent: :destroy
+  enum stage: [ :axised, :headed, :grouped, :imported_kids ]
 end
