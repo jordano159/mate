5af6bf78b4ad782de083c889356143aedfc66e29
Jordan Shefner <yarden11111@gmail.com>
2019-01-26 17:18:35 +0200

    Better grouped in wizard

--- app/controllers/mifal_steps_controller.rb
+++ app/controllers/mifal_steps_controller.rb
@@ -35,9 +35,9 @@ class MifalStepsController < ApplicationController
         head_nums << params[:mifal]["head_num_#{i}"].to_i
       end
       counter = 1
-      @mifal.axes.each_with_index do |axis,i| # ?לאחד את שתי הלולאות
-        head_nums[i].times do |j|
-          Head.create(name: "ראש #{counter} #{@mifal.name}", axis_id: axis.id)
+      @mifal.axes.each_with_index do |axis,i|
+        head_nums[i].times do
+          Head.create(name: "ראש #{counter} #{@mifal.name}", axis_id: axis.id) if Head.find_by(name: "ראש #{counter} #{@mifal.name}").nil?
           next unless Staff.find_by(username: "ראש #{counter} #{@mifal.name}").nil?
 
           Staff.create(name: "ראשראשית #{counter} #{@mifal.name}", email: "h#{@mifal.name}#{counter}@gmail.com", password: '123123',
@@ -48,16 +48,21 @@ class MifalStepsController < ApplicationController
       end
     when :grouped
       @mifal.grouped! # Set stage
-      group_num = params[:mifal][:group_num].to_i
-      group_in_head_num = params[:mifal][:group_in_head_num].to_i
-      (1..group_num).each do |i|
-        Group.create!(name: "קבוצה #{i} #{@mifal.name}",
-          head_id: Head.find_by(name: "ראש #{(i / (group_in_head_num + 1)) + 1} #{@mifal.name}").id.to_s) if Group.find_by(name: "קבוצה #{i} #{@mifal.name}").nil?
-        next unless Staff.find_by(username: "קבוצה #{i} #{@mifal.name}").nil?
+      group_nums = []
+      @mifal.heads.count.times do |i|
+        group_nums << params[:mifal]["group_num_#{i}"].to_i
+      end
+      counter = 1
+      @mifal.heads.each_with_index do |head,i|
+        group_nums[i].times do
+          Group.create(name: "קבוצה #{counter} #{@mifal.name}", head_id: head.id) if Group.find_by(name: "קבוצה #{counter} #{@mifal.name}").nil?
+          next unless Staff.find_by(username: "קבוצה #{counter} #{@mifal.name}").nil?
 
-        Staff.create!(name: "מדריכת #{i} #{@mifal.name}", email: "g #{@mifal.name}#{i}@gmail.com", password: '321321', password_confirmation: '321321',
-                      role: 'user', username: "קבוצה #{i} #{@mifal.name}", staffable_type: 'Group',
-                      staffable_id: Group.find_by(name: "קבוצה #{i} #{@mifal.name}").id)
+          Staff.create(name: "מדריכת #{counter} #{@mifal.name}", email: "g #{@mifal.name}#{counter}@gmail.com", password: '321321', password_confirmation: '321321',
+                        role: 'user', username: "קבוצה #{counter} #{@mifal.name}", staffable_type: 'Group',
+                        staffable_id: Group.find_by(name: "קבוצה #{counter} #{@mifal.name}").id)
+          counter += 1
+        end
       end
     end
     @mifal.update(mifal_params)
