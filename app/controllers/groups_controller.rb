34d7ab57f933fbe4e617eb747ff6652f87cc7444
Jordan Shefner <yarden11111@gmail.com>
2019-02-22 15:07:33 +0200

    Tiny fix

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
     elsif current_staff.admin?
       @groups = Group.all.order('id ASC').includes(:kids)
     else
-      @groups = current_staff.staffable.groups
+      @groups = current_staff.staffable.groups.order('id ASC')
     end
   end
 
