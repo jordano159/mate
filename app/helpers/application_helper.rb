25b634083878de8d87982be73748486337c474db
Jordan Shefner <yarden11111@gmail.com>
2019-07-11 14:51:43 +0300

    Add call in kid info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,23 @@ module ApplicationHelper
     end
     counter
   end
+
+  def mobile_device?
+     if session[:mobile_param]
+       session[:mobile_param] == "1"
+    else
+        request.user_agent =~ /Mobile|webOS/
+    end
+  end
+
+  def error_btn
+    html = "<hr style='width:75%;border: 1px solid #EEE;' />"
+    if mobile_device?
+      html += "<a href='whatsapp://send?text=דיווח על בעיה בשולחן קליטה&phone=972558831115' data-action='share/whatsapp/share' class='btn btn-warning btn-block'>דיווח על בעיה או תקלה</a>"
+    else
+      html += mail_to 'itay.ariely@gmail.com', 'דיווח על בעיה או תקלה', subject: 'דיווח על בעיה או תקלה בשולחן קליטה', class:'btn btn-warning btn-block'
+    end
+    html.html_safe
+  end
+  
 end
