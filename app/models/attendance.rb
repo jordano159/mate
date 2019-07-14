e860e61f426dafd03ee58e2c78307b657a598511
Jordan Shefner <yarden11111@gmail.com>
2019-07-14 12:47:25 +0300

    Little fix to export mifal

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -5,4 +5,11 @@ class Attendance < ApplicationRecord
   belongs_to :check
   validates :cause, exclusion: { in: %w[אחר],
                                  message: 'חובה לפרט סיבת היעדרות' }
+   def heb_status
+     if status == 1
+       string1 = "נוכח/ת"
+     else
+       string1 = "לא נוכח/ת"
+     end
+   end
 end
