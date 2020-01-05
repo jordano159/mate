a2fbed995d48bb09c60c58868d3a304a5a287903
Jordan <yarden11111@gmail.com>
2020-01-05 19:30:40 +0200

    Add contact and fix remember me

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :contacts
   # get 'landing/index'
   resources :landing, only: [:index, :new, :create]
   resources :buses
